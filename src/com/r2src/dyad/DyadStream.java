77c1d8349af2fd9450bf07a442d1119cf7c406d6
Jolanda Verhoef <jolanda@r2src.com>
2012-06-05 15:47:19 +0200

    messed up moving files, committing the removal of old locations. sorry

--- src/com/r2src/dyad/DyadStream.java
+++
@@ -1,5 +0,0 @@
-package com.r2src.dyad;
-
-public class DyadStream {
-
-}
--- src/com/r2src/dyad/Foo.java
+++
@@ -1,7 +0,0 @@
-package com.r2src.dyad;
-
-public interface Foo {
-	public void onFinished();
-	public void onError(Exception e);
-	public void onCanceled();
-}
