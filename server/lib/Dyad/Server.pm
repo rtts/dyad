a10e0ea09cf60f7bd715541965f6900a70cb1018
Jolanda Verhoef <jolanda@r2src.com>
2012-06-06 00:55:57 +0200

    casual server progress

--- server/lib/Dyad/Server.pm
+++ server/lib/Dyad/Server.pm
@@ -3,32 +3,101 @@ package Dyad::Server;
 use 5.014002;
 use strict;
 use warnings;
+use FCGI;
+use JSON;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Dyad::Server ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+#require Exporter;
+#
+#our @ISA = qw(Exporter);
+#
+## Items to export into callers namespace by default. Note: do not export
+## names by default without a very good reason. Use EXPORT_OK instead.
+## Do not simply export all your public functions/methods/constants.
+#
+## This allows declaration	use Dyad::Server ':all';
+## If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+## will save memory.
+#our %EXPORT_TAGS = (
+#	'all' => [
+#		qw(
+#
+#		  )
+#	]
+#);
+#
+#our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw(
-	
+
 );
+
+# TODO: Use Error.pm to throw exceptions!
+
 
 our $VERSION = '0.01';
 
+my $Routing_table = { '/register' => \&register };
 
-# Preloaded methods go here.
+sub respond;
+sub get_body;
+
+sub new {
+	my $class = shift;
+	my $self  = {};
+	$self->{SOCKET} = FCGI::OpenSocket( shift, 100 );
+	$self->{REQUEST} =
+	  FCGI::Request( \*STDIN, \*STDOUT, \*STDERR, \%ENV, $self->{SOCKET} );
+	bless $self, $class;
+	return $self;
+}
+
+sub DESTROY {
+	my $self = shift;
+	FCGI::CloseSocket $self->{SOCKET};
+}
+
+sub run {
+	my $self = shift;
+  client: while ( $self->{REQUEST}->Accept() >= 0 ) {
+
+		#print "Content-type: text/plain\r\n\r\n";
+
+		#for ( keys %ENV ) {
+		#	print "$_: $ENV{$_}\n";
+		#}
+		my $func = $Routing_table->{ $ENV{REQUEST_URI} };
+		respond 404, "unknown API request" if not $func;
+		print $func->();
+	}
+}
+
+sub register {
+	return respond 405, "expecting POST request"
+	  if not $ENV{'REQUEST_METHOD'} eq 'POST';
+	my $result = get_body;
+
+	my $token   = $_->{"token"}   or respond 422, "token missing";
+	my $c2dm_id = $_->{"c2dm_id"} or respond 422, "c2dm_id missing";
+
+}
+
+sub get_body {
+	return respond 400, "no body found in request"
+	  if not $ENV{'CONTENT_LENGTH'};
+
+	my $length = read( STDIN, my $body, $ENV{'CONTENT_LENGTH'} );
+	return respond 400, "reported content length does not match actual length"
+	  if $ENV{'CONTENT_LENGTH'} != $length;
+
+	return ( ( decode_json $body)
+		  or ( respond 400, "json could not be decoded" ) );
+}
+
+sub respond {
+	my $status = shift;
+	my $body   = shift;
+	return "Status: $status\r\nContent-type: text/plain\r\n\r\n$body";
+}
 
 1;
 __END__
@@ -41,8 +110,9 @@ Dyad::Server - Perl extension for blah blah blah
 =head1 SYNOPSIS
 
   use Dyad::Server;
-  blah blah blah
-
+  my $server = Dyad::Server->new("127.0.0.1:8000");
+  $server->start();
+  
 =head1 DESCRIPTION
 
 Stub documentation for Dyad::Server, created by h2xs. It looks like the
--- server/t/Dyad-Server.t
+++ server/t/Dyad-Server.t
@@ -8,7 +8,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 4;
 BEGIN { use_ok('Dyad::Server') };
 
 #########################
@@ -16,3 +16,11 @@ BEGIN { use_ok('Dyad::Server') };
 # Insert your test code below, the Test::More module is use()ed here so read
 # its man page ( perldoc Test::More ) for help writing this test script.
 
+my $server = Dyad::Server->new("localhost:3454");
+isa_ok $server, 'Dyad::Server';
+ok defined $server->{SOCKET};
+ok defined $server->{REQUEST};
+
+
+#$ENV{CONTENT_LENGTH} = 10;
+#$server->get_body();
